9d2c496f55608533adc8813d39b94118f029658c
LeoLacana <wilson17414@gmail.com>
2021-07-27 13:20:56 -0300

    refatorado contextAPI exercise-one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './context/MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.handleMoveCar = this.handleMoveCar.bind(this);
+  }
+
+  handleMoveCar(car, side) {
+    this.setState({ 
+      cars: { [ car ]: side }
+    });
+  }
+
+  render() {
+    const contextState = {
+      cars: this.state.cars,
+      moveCar: this.handleMoveCar,
+    }
+    return (
+      <MyContext.Provider value={ contextState }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
